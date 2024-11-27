c5cb6b0553bb981ebd315d2422b07a5ec0df4f7d
Matheus Mello <mellomatheuslima@gmail.com>
2024-11-26 23:10:23 -0300

    Delete legacy API backend

--- petin-api/src/infrastructure/config/session.config.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export interface SessionConfig {
-  secret: string;
-}
-
-export default registerAs('session', () => ({
-  secret: process.env.SESSION_SECRET,
-}));
