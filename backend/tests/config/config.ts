2ac02ded17e9e6ea7fa2898934a26aa4a10be7a6
Matheus Lima <matheus.lima@wexinc.com>
2024-12-03 20:36:05 -0300

    Fix GitHub workflows errors

--- backend/tests/config/config.ts
+++ backend/tests/config/config.ts
@@ -1,2 +1,3 @@
-export const url = "http://localhost:3000";
+export const port = process.env.API_PORT || 3000;
+export const url = `http://localhost:${port}`;
 export const databaseUrl = "postgres://postgres:password@postgres:5432/petin";
