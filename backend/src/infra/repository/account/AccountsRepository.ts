8c9a920710d6046c21cbfb0b0085989c923993e2
Matheus Lima <matheus.lima@wexinc.com>
2024-12-03 20:17:58 -0300

    Fix Dependency Injection to not use old code

--- backend/src/infra/repository/account/AccountsRepository.ts
+++ backend/src/infra/repository/account/AccountsRepository.ts
@@ -1,8 +1,4 @@
 import { LoginRepository } from "../../../application/account/usecase/Login";
 import { SignupRepository } from "../../../application/account/usecase/Signup";
-import { CreateOwnerAccountsRepository } from "../../../application/owner/usecase/CreateOwner";
 
-export interface AccountsRepository
-  extends LoginRepository,
-    SignupRepository,
-    CreateOwnerAccountsRepository {}
+export interface AccountsRepository extends LoginRepository, SignupRepository {}
