aae2464887219bffec9e12366a24837e3f416037
Matheus Lima <mellomatheuslima@gmail.com>
2024-12-18 01:38:45 -0300

    Update host to localhost when testing

--- backend/tests/config/config.ts
+++ backend/tests/config/config.ts
@@ -4,7 +4,7 @@ export type TestEnvironment = {
 
 export function setupEnvironmentVariables(): TestEnvironment {
   const port = process.env.API_PORT || 3100;
-  const host = process.env.API_HOST || "127.0.0.1";
+  const host = process.env.API_HOST || "localhost";
   const url = `http://${host}:${port}`;
 
   return {
